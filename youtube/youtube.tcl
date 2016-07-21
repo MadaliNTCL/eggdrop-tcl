086771d11367305cb3c673bd5f4e232fdedd0018
MadaliN <madalinmen28@gmail.com>
2016-07-21 10:55:45 +0300

    Create youtube.tcl

---
+++ youtube/youtube.tcl
@@ -0,0 +1,92 @@
+# +-------------------------------------------------------------------------------------+
+# |                                                                                     |
+# |                         YouTUBE Script v1.0.0                                       |
+# |                                                                                     |
+# +-------------------------------------------------------------------------------------+
+# |                                                                                     |
+# | *** Website             @  http://www.EggdropTCL.com                                |
+# | *** GitHub              @  http://github.com/MadaliNTCL/eggdrop-tcl                 |
+# |                                                                                     |
+# +-------------------------------------------------------------------------------------+
+# | *** IRC Support:                                                                    |
+# |                    #EggdropTCL     @ QuakeNET                                       |
+# |                    #EggdropTCL     @ UnderNET                                       |
+# |                    #EggdropTCL     @ EfNET                                          |
+# |                                                                                     |
+# | *** Contact:                                                                        |
+# |                    Yahoo Messenger/Mail: madalinmen28@yahoo.com                     |
+# |                    Google Mail         : madalinmen28@gmail.com                     |
+# |                    Skype Messenger     : madalinmen28                               |
+# |                                                                                     |
+# +-------------------------------------------------------------------------------------+
+# | IMPORTANT:                                                                          |
+# |                                                                                     |
+# | There is no need for script activation                                              | 
+# | Its active on every channel                                                         |
+# | Command can be used once 10 seconds for each user                                   |
+# +-------------------------------------------------------------------------------------+
+
+bind PUBM - * youtube
+
+package require json
+package require http
+package require tls
+
+set youtube(api) ""
+
+proc youtube {nick uhost hand chan arg} {
+	global ytignore youtube
+
+	if {![string match -nocase *yout* $arg]} { return 0 }
+
+	## ++
+	set floodtime 10
+
+	## ++ 
+	if {![info exists ytignore($nick)]} {
+		set ytignore($nick) [unixtime]
+		utimer $floodtime [list unset -nocomplain ytignore($nick)]
+	}
+
+	## ++ 
+	if {[expr [unixtime]-$ytignore($nick)]>$floodtime} { putlog "ignoram"; return 0 }
+
+	set youtubecheck [regexp -all -nocase {(?:\/watch\?v=|youtu\.be\/)([\d\w-]{11})} $arg match youtubeid]
+	
+	::http::register https 443 ::tls::socket
+
+	if {[catch {http::geturl "https://www.googleapis.com/youtube/v3/videos?[http::formatQuery id $youtubeid key $youtube(api) part snippet,contentDetails,statistics,status]"} tok]} {
+		putlog "Socket error: $tok"
+		return 0
+	}
+	if {[http::status $tok] ne "ok"} {
+		set status [http::status $tok]
+		
+		putlog "TCP error: $status"
+		return 0
+	}
+	if {[http::ncode $tok] != 200} {
+		set code [http::code $tok]
+		http::cleanup $tok
+
+		putlog "HTTP Error: $code"
+		return 0
+	}
+
+	set data [http::data $tok]
+
+	set parse [::json::json2dict $data]
+	
+	set playtime [lindex [dict get [lindex [dict get $parse items] 0] snippet] 1]
+	set title [lindex [dict get [lindex [dict get $parse items] 0] snippet] 5]
+	set viewCount [lindex [dict get [lindex [dict get $parse items] 0] statistics] 1]
+	set likeCount [lindex [dict get [lindex [dict get $parse items] 0] statistics] 3]
+	set dislikeCount [lindex [dict get [lindex [dict get $parse items] 0] statistics] 5]
+	set commentCount [lindex [dict get [lindex [dict get $parse items] 0] statistics] 9]
+
+	putserv "PRIVMSG $chan :\002\00301,00You\00300,04Tube\002\017 \00312$title\003 \037\002/\002\037 \00302Views\003: \00303[youtube:convert $viewCount]\003 \037\002/\002\037 \00302Likes\003: \00310[youtube:convert $likeCount]\003 \037\002/\002\037 \00302Dislikes\003: \00304[youtube:convert $dislikeCount]\003 \037\002/\002\037 \00302Comments\003: \00304[youtube:convert $commentCount]\003"
+}
+
+proc youtube:convert {num} { while {[regsub {^([-+]?\d+)(\d\d\d)} $num "\\1.\\2" num]} {}; return $num }
+
+putlog "Succesfully loaded: \00303YouTUBE TCL Script"
