77847dabc838925d34d463a8c17bf2d18fd42b53
MadaliN <madalinmen28@gmail.com>
2016-08-23 16:15:31 +0300

    Update myshows.tcl

--- myshows/myshows.tcl
+++ myshows/myshows.tcl
@@ -75,20 +75,26 @@ proc myshows:tv {nick uhost hand chan arg} {
 
 			set result [dict get $connect result]
 
-			set temp(canceled) ""; set temp(running) ""
+			set temp(canceled) ""; set temp(running) ""; set temp(pause) ""
 
 			foreach line $result {
-				if {[string match -nocase *ended* [dict get $line status]]} { lappend temp(canceled) "\00304[dict get $line id]\003 - \00313[dict get $line titleOriginal]" }
+				if {[string match -nocase *TBD* [dict get $line status]]} { lappend temp(pause) "\00304[dict get $line id]\003 - \00306[dict get $line titleOriginal]" }				
+				if {[string match -nocase *ended* [dict get $line status]]} { lappend temp(canceled) "\00304[dict get $line id]\003 - \00306[dict get $line titleOriginal]" }
 				if {[string match -nocase *returning* [dict get $line status]]} { lappend temp(running) "\00312[dict get $line id]\003 - \00303[dict get $line titleOriginal]" }
 			}
 
-			putserv "PRIVMSG $chan :\002Running\002: [join $temp(running) ", "]"
-			putserv "PRIVMSG $chan :\002Canceled/Ended\002: [join $temp(canceled) ", "]"
+			if {$temp(running) ne ""} { putquick "PRIVMSG $chan :\002Running\002: [join $temp(running) ", "]" }
+			if {$temp(pause) ne ""} { putquick "PRIVMSG $chan :\002Pause\002: [join $temp(pause) ", "]" }						
+			if {$temp(canceled) ne ""} { putquick "PRIVMSG $chan :\002Canceled/Ended\002: [join $temp(canceled) ", "]" }
+			
+			if {$temp(running) ne "" || $temp(pause) ne "" || $temp(canceled) ne ""} { putquick "PRIVMSG $chan :\002$nick\002 - You can now see informations using command \00306!tv \00304id" }
+			
+			if {$result eq ""} { putquick "PRIVMSG $chan :\002$nick\002 - Nothing found"; return }
 		}
-		default { 
-				if {[isnumber [lindex [split $arg] 0]]} { shows.GetById $chan [lindex [split $arg] 0]; return}
-				
-				putquick "PRIVMSG $chan :\002$nick\002 - \002USAGE\002: !tv <search/\[\$id\]> \[\$name\]"
+		default {
+			if {[isnumber [lindex [split $arg] 0]]} { shows.GetById $chan [lindex [split $arg] 0]; return}
+
+			putquick "PRIVMSG $chan :\002$nick\002 - \002USAGE\002: !tv <search/\[\$id\]> \[\$name\]"
 		}
 	}
 }
@@ -113,26 +119,44 @@ proc shows.GetById {chan id} {
 
 	set temp(episode-list) [dict get [dict get $connect result] episodes]
 
-	putserv "PRIVMSG $chan :\00312[dict get [dict get $connect result] titleOriginal]\003 \037\002/\002\037 \00302Status: \00304[dict get [dict get $connect result] status]\003 \037\002/\002\037 \00302Total Season: \00303\002[dict get [dict get $connect result] totalSeasons]\002\003 - \00306https://en.myshows.me/view/[dict get [dict get $connect result] id]/"
-	putserv "PRIVMSG $chan :\00302Premiered\003: \00303[dict get [dict get $connect result] started]\003 - \00304\002[dict get [dict get $connect result] ended]\002\003 \037\002/\002\037 \00302Rating: \00304[dict get [dict get $connect result] rating]\003 (Votes: \00304[dict get [dict get $connect result] voted]\003) \037\002/\002\037 \00302Watchers: \00304[dict get [dict get $connect result] watching]\003 \037\002/\002\037 \00302Country: \00304[dict get [dict get $connect result] country]\003"
+	putquick "PRIVMSG $chan :\00312[dict get [dict get $connect result] titleOriginal]\003 \037\002/\002\037 \00302Status: \00304[dict get [dict get $connect result] status]\003 \037\002/\002\037 \00302Total Season: \00303\002[dict get [dict get $connect result] totalSeasons]\002\003 - \00306https://en.myshows.me/view/[dict get [dict get $connect result] id]/"
+	putquick "PRIVMSG $chan :\00302Premiered\003: \00303[dict get [dict get $connect result] started]\003 - \00304\002[dict get [dict get $connect result] ended]\002\003 \037\002/\002\037 \00302Rating: \00304[dict get [dict get $connect result] rating]\003 (Votes: \00304[dict get [dict get $connect result] voted]\003) \037\002/\002\037 \00302Watchers: \00304[dict get [dict get $connect result] watching]\003 \037\002/\002\037 \00302Country: \00304[dict get [dict get $connect result] country]\003"
 
-	# ++ Next episode
-	set nextepisode [lindex $temp(episode-list) 0]; set date [lindex [string map [list "T" " " "+0000" ""] [dict get $nextepisode airDate]] 0]
-													set final [duration [expr [unixtime] - [clock scan $date]]]
-													
+	set temp(next5) ""; set temp(last5) ""
 
-	putserv "PRIVMSG $chan :\002#1\002 \00302next episode"
-	putserv "PRIVMSG $chan :\00304[dict get $nextepisode shortName]\003 - \00312[dict get $nextepisode title]\003 airs in \002$final\002 (\00312$date\003)"
+	foreach episode $temp(episode-list) {
+		set airDate [expr [clock scan [string map [list "T" " " "+0000" ""] [dict get $episode airDate]]] - [unixtime]]
+		set shortName [dict get $episode shortName]
+		set title [dict get $episode title]
 
-	# ++ Last 3 episodes
-	set 1 [lindex $temp(episode-list) 1]; set date1 [lindex [string map [list "T" " " "+0000" ""] [dict get $1 airDate]] 0]; set final1 [duration [expr [unixtime] - [clock scan $date1]]]
-	set 2 [lindex $temp(episode-list) 2]; set date2 [lindex [string map [list "T" " " "+0000" ""] [dict get $2 airDate]] 0]; set final2 [duration [expr [unixtime] - [clock scan $date2]]]
-	set 3 [lindex $temp(episode-list) 3]; set date3 [lindex [string map [list "T" " " "+0000" ""] [dict get $3 airDate]] 0]; set final3 [duration [expr [unixtime] - [clock scan $date3]]]
+		if {![string match -nocase *-* $airDate]} {
+			lappend temp(next5) "[list $airDate $shortName $title]"
+		} else {
+			lappend temp(last5) "[list $airDate $shortName $title]"
+		}
+	}
 
-	putserv "PRIVMSG $chan :\002#3\002 \00302last episodes"
-	putserv "PRIVMSG $chan :\00304[dict get $1 shortName]\003 - \00312[dict get $1 title]\003 aired $final1 ago (\00312$date1\003)"
-	putserv "PRIVMSG $chan :\00304[dict get $2 shortName]\003 - \00312[dict get $2 title]\003 aired $final2 ago (\00312$date2\003)"
-	putserv "PRIVMSG $chan :\00304[dict get $3 shortName]\003 - \00312[dict get $3 title]\003 aired $final3 ago (\00312$date3\003)"
+	set lsortnext [lsort -index 0 -integer -increasing $temp(next5)]
+	set lsortlast [lsort -index 0 -integer -decreasing $temp(last5)]
+
+	set next 0; set last 0
+
+	foreach line $lsortnext {
+		incr next
+
+		if {$next <= "3"} { putquick "PRIVMSG $chan :\002\037\[\002\037NexT\037\002\]\037\002 \00302Name\003: \00312[lindex $line 2]\003 - \00304[lindex $line 1]\003 airs in \00303[convert:myshows [duration [lindex $line 0]]]" }
+	}
+
+	foreach line $lsortlast {
+		incr last
+
+		set airDate [string map [list "-" ""] [lindex $line 0]]
+		if {$last <= "3"} { putquick "PRIVMSG $chan :\002\037\[\002\037LasT\037\002\]\037\002 \00302Name\003: \00312[lindex $line 2]\003 - \00304[lindex $line 1]\003 airs in \00303[convert:myshows [duration $airDate]]" }
+	}
 }
 
+proc convert:myshows {arg} {
+
+	return [string map [list " years" "y" " year" "y" " months" "m" " month" "m" " weeks" "w" " week" "w" " days" "d" " day" "d" " hours" "h" " hour" "h" " minutes" "m" " minute" "m" " seconds" "s" " second" "s"] $arg]
+}
 putlog "++ \[ - \00304PUBLIC\003 - \00306loaded\003 * \00303MyShows\003 \]"
