fcd613200537860417769caee35e22f099658258
MadaliN <madalinmen28@gmail.com>
2020-05-12 14:02:11 +0300

    Update radio.tcl

--- radio/radio.tcl
+++ radio/radio.tcl
@@ -23,16 +23,67 @@
 ######
 
 bind PUBM - * radio:main
-bind PUBM - * radio:commands
+bind PUBM - * ascenture:radio
+
+catch {bind TIME - * radio:routine}
 
 setudef flag radio
 setudef str name
-setudef str ip
+setudef str ips
 setudef str port
+setudef str dj
+
+set temp(cmds) "dj listeners unique peak on maxim frequence song"
 
 package require http
 
+proc radio:routine {min hour day month year} {
+	global temp
+
+	foreach chan [channels] {
+		if {[channel get $chan radio]} {
+			set ip [channel get $chan ips]
+			set port [channel get $chan port]
+		}
+	}
+
+	if {$ip eq ""} { return }
+
+	if {[llength $ip] && [llength $port]} {
+		set url "http://$ip:$port/7.html"
+
+		set token [http::config -useragent Mozilla]
+		set token [http::geturl "$url"]
+		set data [::http::data $token]
+		::http::cleanup $token
+
+		if {[channel get $chan name] == ""} { channel set $chan name "RADIO NAME IS NOT SET" }
+
+		regexp -line {<html><body>(.*),(.*),(.*),(.*),(.*),(.*),(.*)</body></html>$} $data -> unique on peak maxim listeners frequence song;
+
+		if {[info exists temp(song)]} {
+			if {$temp(song) ne $song} {
+				foreach chan [channels] {
+					if {[channel get $chan radio]} {
+						putserv "PRIVMSG $chan :NEW song .. $song"
+
+						set temp(song) $song
+					}
+				}
+			}
+		} else {
+			foreach chan [channels] {
+				if {[channel get $chan radio]} {
+					putserv "PRIVMSG $chan :NEW song .. $song"
+				}
+			}
+			set temp(song) $song
+		}
+	}
+}
+
 proc radio:main {nick uhost hand chan arg} {
+	global temp
 
 	if {[string index $arg 0] in {! . `}} {
 		set temp(cmd) [string range $arg 1 end]
@@ -45,7 +96,7 @@ proc radio:main {nick uhost hand chan arg} {
 
 	set temp(announce) "$nick $temp(cmd) $chan"
 
-	if {[info commands ascenture:$temp(cmd)] != ""} { ascenture:$temp(cmd) $nick $uhost $hand $chan $arg }
+	if {[lsearch -nocase $temp(cmds) $temp(cmd)] ne "-1"} { command $chan $nick $temp(cmd) }
 }
 
 proc ascenture:radio {nick uhost hand chan arg} {
@@ -53,37 +104,11 @@ proc ascenture:radio {nick uhost hand chan arg} {
 
 	if {![matchattr $hand n]} { return }
 
-	set ip [channel get $chan ip]
+	set ip [channel get $chan ips]
 	set port [channel get $chan port]
-	set type [lindex [split $arg] 0]
+	set type [lindex [split $arg] 1]
 
-	switch -exact -- $type {
-		unique {
-			if {[llength $ip] && [llength $port]} {
-				set url "http://$ip:$port/7.html"
-
-				set token [http::config -useragent Mozilla]
-				set token [http::geturl "$url"]
-				set data [::http::data $token]
-				::http::cleanup $token
-
-				if {[channel get $chan name] == ""} { channel set $chan name "RADIO NAME IS NOT SET" }
-
-				regexp -line {<HTML><meta http-equiv="Pragma" content="no-cache"></head><body>(.*),(.*),(.*),(.*),(.*),(.*),(.*)</body></html>$} $data -> unique on peak maxim listeners frequence song;
-
-				if {$unique == ""} { putserv "PRIVMSG $chan :\002$nick\002 - '$type' couldnt be read (the website is not providing this information)"; return }
-
-				putserv "PRIVMSG $chan :\002$nick\002 - The unique number of listeners from the servers are: \00304$unique"
-			}
-		}
-		on { return }
-		peak { return }
-		maxim { return }
-		listeners { return }
-		frequence { return }
-		song { return }
-	}
-	switch -exact [lindex [split $arg] 0] {
+	switch -exact $type {
 		-activate {
 			channel set $chan +radio
 
@@ -95,35 +120,46 @@ proc ascenture:radio {nick uhost hand chan arg} {
 
 			putserv "PRIVMSG $chan :\002$nick\002 - \00304radio TCL\003 has been succesfully \002dezactivat\002 deactivated"
 		}
+		-dj -
+		-dj {
+			if {![llength [lindex [split $arg] 2]]} {
+				putserv "PRIVMSG $chan :\002ERROR\002: You have to specify a 'dj name'"
+				return
+			} else {
+				channel set $chan dj [lindex [split $arg] 2]
+
+				putserv "PRIVMSG $chan :\002SUCCESS\002: 'dj name' has been set to: \00312[lindex [split $arg] 2]"
+			}
+		}
 		-name -
 		-nume {
-			if {![llength [lindex [split $arg] 1]]} {
+			if {![llength [lindex [split $arg] 2]]} {
 				putserv "PRIVMSG $chan :\002ERROR\002: You have to specify a 'radio name'"
 				return
 			} else {
-				channel set $chan name [lindex [split $arg] 1]
+				channel set $chan name [lindex [split $arg] 2]
 
-				putserv "PRIVMSG $chan :\002SUCCESS\002: 'radio name' has been set to: \00312[lindex [split $arg] 1]"
+				putserv "PRIVMSG $chan :\002SUCCESS\002: 'radio name' has been set to: \00312[lindex [split $arg] 2]"
 			}
 		}
 		-ip {
-			if {![llength [lindex [split $arg] 1]]} {
+			if {![llength [lindex [split $arg] 2]]} {
 				putserv "PRIVMSG $chan :\002ERROR\002: You have to specify a 'radio ip'"
 				return
-			} else {
-				channel set $chan ip [lindex [split $arg] 1]
+			} 
+				channel set $chan ips [lindex [split $arg] 2]
 
-				putserv "PRIVMSG $chan :\002SUCCESS\002: 'radio ip' has been set to: \00312[lindex [split $arg] 1]"
-			}
+				putserv "PRIVMSG $chan :\002SUCCESS\002: 'radio ip' has been set to: \00312[lindex [split $arg] 2]"
+
 		}
 		-port {
-			if {![llength [lindex [split $arg] 1]]} {
+			if {![llength [lindex [split $arg] 2]]} {
 				putserv "PRIVMSG $chan :\002ERROR\002: You have to specify a 'radio port'"
 				return
 			} else {
-				channel set $chan port [lindex [split $arg] 1]
+				channel set $chan port [lindex [split $arg] 2]
 
-				putserv "PRIVMSG $chan :\002SUCCESS\002: 'radio port' has been set to: \00312[lindex [split $arg] 1]"
+				putserv "PRIVMSG $chan :\002SUCCESS\002: 'radio port' has been set to: \00312[lindex [split $arg] 2]"
 			}
 		}
 		usage {
@@ -132,22 +168,9 @@ proc ascenture:radio {nick uhost hand chan arg} {
 	}
 }
 
-proc radio:commands {nick uhost hand chan arg} {
+proc command {chan nick type} {
 
-	switch -exact -- [lindex [split $arg] 1] {
-		unique { radio $chan $nick unique }
-		on { radio $chan $nick on }
-		peak { radio $chan $nick peak }
-		maxim { radio $chan $nick maxim }
-		listeners { radio $chan $nick listeners }
-		frequence { radio $chan $nick frecquence }
-		song { radio $chan $nick song }
-	}
-}
-
-proc radio {chan nick type} {
-
-	set ip [channel get $chan ip]
+	set ip [channel get $chan ips]
 	set port [channel get $chan port]
 
 	if {[llength $ip] && [llength $port]} {
@@ -160,10 +183,10 @@ proc radio {chan nick type} {
 
 		if {[channel get $chan name] == ""} { channel set $chan name "RADIO NAME IS NOT SET" }
 
-		regexp -line {<HTML><meta http-equiv="Pragma" content="no-cache"></head><body>(.*),(.*),(.*),(.*),(.*),(.*),(.*)</body></html>$} $data -> unique on peak maxim listeners frequence song;
+		regexp -line {<html><body>(.*),(.*),(.*),(.*),(.*),(.*),(.*)</body></html>$} $data -> unique on peak maxim listeners frequence song;
 
 		switch -exact -- $type {
-			unique {
+			unici - unique {
 				if {$unique == ""} { putserv "PRIVMSG $chan :\002$nick\002 - '$type' couldnt be read (the website is not providing this information)"; return }
 
 				putserv "PRIVMSG $chan :\002$nick\002 - The unique number of listeners from the servers are: \00304$unique"
@@ -173,7 +196,7 @@ proc radio {chan nick type} {
 
 				putserv "PRIVMSG $chan :\002$nick\002 - Online listeners on the server are: \00304$on"
 			}
-			peak {
+			varf - peak {
 				if {$peak == ""} { putserv "PRIVMSG $chan :\002$nick\002 - '$type' couldnt be read (the website is not providing this information)"; return }
 
 				putserv "PRIVMSG $chan :\002$nick\002 - current peak is: \00304$peak"
@@ -183,20 +206,24 @@ proc radio {chan nick type} {
 
 				putserv "PRIVMSG $chan :\002$nick\002 - The maxim number of listeners that can connect to the servers is: \00304$maxim"
 			}
-			listeners {
+			ascultatori - listeners {
 				if {$listeners == ""} { putserv "PRIVMSG $chan :\002$nick\002 - '$type' couldnt be read (the website is not providing this information)"; return }
 
 				putserv "PRIVMSG $chan :\002$nick\002 - The current listeners connected on the server are: \00304$listeners"
 			}
-			frequence {
+			frecventa - frequence {
 				if {$frequence == ""} { putserv "PRIVMSG $chan :\002$nick\002 - '$type' couldnt be read (the website is not providing this information)"; return }
 
 				putserv "PRIVMSG $chan :\002$nick\002 - The current frequence is: \00304$frequence "
 			}
-			song {
+			melodie - song {
 				if {$song == ""} { putserv "PRIVMSG $chan :\002$nick\002 - '$type' couldnt be read (the website is not providing this information)"; return }
 
 				putserv "PRIVMSG $chan :\002$nick\002 - current song is: \00304$song"
+			}
+			dj {
+
+				putserv "PRIVMSG $chan :\002$nick\002 - DJ is: \00304[channel get $chan dj]"
 			}
 		}
 	}
